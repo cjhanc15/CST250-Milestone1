73792aedc10c06b90900d4f0e6722c751043a024
cjhanc15 <102087890+cjhanc15@users.noreply.github.com>
2023-07-05 09:03:53 -0700

    Add files via upload

---
+++ Milestone1ClassLibrary/Milestone1ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Milestone1ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board(10);
+            board.setupLiveNeighbors();
+            board.calculateLiveNeighbors();
+            printBoard(board);
+        }
+
+        static void printBoard(Board board)
+        {
+            Console.WriteLine("Minesweeper");
+            for (int row = 0; row < board.Size; row++)
+            {
+                for (int col = 0; col < board.Size; col++)
+                {
+                    Cell cell = board.Grid[row, col];
+                    if (cell.Live)
+                    {
+                        Console.Write("X "); // Print 'X' for live cells (bombs)
+                    }
+                    else
+                    {
+                        Console.Write(cell.LiveNeighbors + " "); // Print the number of live neighbors
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
